23725511812eb3ded4a05adf881707e4dd4ec35a
jiguruGit <vamshijiguru2@gmail.com>
2023-04-27 00:55:30 +0530

    ftscomt

---
+++ src/main/java/com/spring/project/EcomasprojectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomasprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomasprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/spring/project/entity/Product.java
@@ -0,0 +1,5 @@
+package com.spring.project.entity;
+
+public class Product {
+
+}
